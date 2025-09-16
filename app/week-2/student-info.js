fa39d473bba5c1c75b61e7cad5f2e82c798d4f0e
Mrinal Jha <jhamrinal63@gmail.com>
2025-09-16 17:48:47 -0600

    finished create and render for student profile

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Mrinal Jha</h1>
+            <a href="https://github.com/Bucket-monkey567/cprg306-assignments">Repo Link</a>          
+        </main>
+    );
+}
