d7ba8138a636c0e1d916f8c5736d0e736fdc11db
Mrinal Jha <jhamrinal63@gmail.com>
2025-09-18 09:04:16 -0600

    added names of otherr group members

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,6 +4,8 @@ export default function StudentInfo() {
     return (
         <main>
             <h1>Mrinal Jha</h1>
+            <h1>Noah Zschogner</h1>
+            <h1> Kiera Johnson </h1>
             <Link href = "https://github.com/Bucket-monkey567/cprg306-assignments" >GitHub Repository Link</Link>
         </main>
     );
