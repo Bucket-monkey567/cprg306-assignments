133d6ffc18dd44b609f6f7abdc90bad37de85b38
Mrinal Jha <jhamrinal63@gmail.com>
2025-09-18 09:01:41 -0600

    replaced a with link

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
+
 export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <a href="/week-2">Week 2</a>
+      <Link href="week-2">Week 2</Link>
     </main>
   );
 }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function StudentInfo() {
     return (
         <main>
             <h1>Mrinal Jha</h1>
-            <a href="https://github.com/Bucket-monkey567/cprg306-assignments">Repo Link</a>          
+            <Link href = "https://github.com/Bucket-monkey567/cprg306-assignments" >GitHub Repository Link</Link>
         </main>
     );
 }
