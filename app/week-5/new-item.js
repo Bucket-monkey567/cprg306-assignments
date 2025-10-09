5986d1478c4846fccc3e0c047666c23c2276bb4a
jhamrinal63@gmail.com <jhamrinal63@gmail.com>
2025-10-08 21:36:58 -0600

    fixed the increase/ decrease button producing the alert + cleane dthe format

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -32,8 +32,9 @@ export default function NewItem(){
 
     return(
         <section>
-            <form onSubmit={handleSubmit} className="m-2.5 p-2 border-2 border-fuchsia-300 rounded-md">
+            {/* Name input */}
                 <input className="m-2.5 p-2 border-2 border-fuchsia-300 rounded-md" type="text" placeholder="Item Name" value={name} onChange={(e) => setName(e.target.value)} />
+            {/* Category dropdown */}
                 <select className="m-2.5 p-2 border-2 border-fuchsia-300 rounded-md bg-black text-white" value={category} onChange={(e) => setCategory(e.target.value)}>
                     <option value="produce">Produce</option>
                     <option value="meat">Meat</option>
@@ -42,11 +43,12 @@ export default function NewItem(){
                     <option value="dry goods">Dry Goods</option>
                     <option value="household">Household</option>
                 </select>
+            {/* Quantity buttons */}
                 <h2 className="m-2.5">Quantity: {quantity}</h2>
                 <button className="bg-fuchsia-300 text-black mx-2 px-2 py-1 rounded-md" onClick={decrement}>Decrease</button>
                 <button className="bg-fuchsia-300 text-black mx-2 px-2 py-1 rounded-md" onClick={increment}>Increase</button>
-                <button className="bg-fuchsia-300 text-black mx-2 px-2 py-1 rounded-md">Submit</button>
-            </form>
+            {/* Submit button */}
+                <button onClick={handleSubmit} className="m-2.5 p-2 border-2 border-fuchsia-300 rounded-md">Submit</button>
         </section>
     )
 }
