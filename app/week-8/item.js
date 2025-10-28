26ca2032682afbb5693af2cbdaba283ade07e580
jhamrinal63@gmail.com <jhamrinal63@gmail.com>
2025-10-27 19:08:39 -0600

    set up week 8 folder

---
+++ app/week-8/item.js
@@ -0,0 +1,10 @@
+export default function Item({name, quantity, category}) {
+    return (
+        <section>
+            <li className="to-blue-300 p-2 m-2 bg-gray-400 rounded-2xl">
+                <p className="font-bold">{name}</p>
+                <p>Buy {quantity} in {category}</p>
+            </li>
+        </section>
+    )
+}
