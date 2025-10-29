145ed2bf87c9fd6a1a9ef00536efd1904c7226a3
jhamrinal63@gmail.com <jhamrinal63@gmail.com>
2025-10-28 22:46:13 -0600

    more css styling to be more user reactive

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -8,7 +8,7 @@ export default function Item({name, quantity, category, onSelect}) {
     return (
         <section>
             <li onClick={handleClick} 
-            className="to-blue-300 p-2 m-2 bg-gray-400 rounded-2xl">
+            className="to-blue-300 p-2 m-2 bg-gray-400 rounded-2xl cursor-pointer hover:bg-gray-200 hover:text-black">
                 <p className="font-bold">{name}</p>
                 <p>Buy {quantity} in {category}</p>
             </li>
