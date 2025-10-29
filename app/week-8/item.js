22b16073c33b40555f59f323ba41a5fdc07ce965
jhamrinal63@gmail.com <jhamrinal63@gmail.com>
2025-10-28 20:01:21 -0600

    test est

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -7,7 +7,7 @@ export default function Item({name, quantity, category}) {
 
     return (
         <section>
-            <li className="to-blue-300 p-2 m-2 bg-gray-400 rounded-2xl">
+            <li onClick={handleClick} className="to-blue-300 p-2 m-2 bg-gray-400 rounded-2xl">
                 <p className="font-bold">{name}</p>
                 <p>Buy {quantity} in {category}</p>
             </li>
